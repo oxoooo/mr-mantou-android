4f58527f824060974fdc14894e94047479ee4fbe
XiNGRZ <chenxingyu92@gmail.com>
2015-10-16 18:23:37 +0800

    image sharing

--- app/src/main/java/ooo/oxo/mr/ViewerFragment.java
+++ app/src/main/java/ooo/oxo/mr/ViewerFragment.java
@@ -21,12 +21,17 @@ package ooo.oxo.mr;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.transition.Transition;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -35,12 +40,22 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.target.Target;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.ExecutionException;
+
 import ooo.oxo.mr.databinding.ViewerFragmentBinding;
 import ooo.oxo.mr.model.Image;
 import ooo.oxo.mr.net.GlideRequestListenerAdapter;
 import ooo.oxo.mr.util.EnterTransitionCompat;
 import ooo.oxo.mr.util.SimpleTransitionListener;
 import ooo.oxo.mr.widget.RxBindingFragment;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class ViewerFragment extends RxBindingFragment<ViewerFragmentBinding> {
 
@@ -50,6 +65,8 @@ public class ViewerFragment extends RxBindingFragment<ViewerFragmentBinding> {
     private String thumbnail;
 
     private boolean hasSharedElementTransition = false;
+
+    private boolean isTransitionExecuted = false;
 
     private View sharedElement;
 
@@ -64,6 +81,8 @@ public class ViewerFragment extends RxBindingFragment<ViewerFragmentBinding> {
 
         hasSharedElementTransition = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP &&
                 !TextUtils.isEmpty(thumbnail);
+
+        setHasOptionsMenu(true);
     }
 
     @Nullable
@@ -83,7 +102,33 @@ public class ViewerFragment extends RxBindingFragment<ViewerFragmentBinding> {
 
         sharedElement = binding.thumbnail;
 
+        if (savedInstanceState != null) {
+            isTransitionExecuted = savedInstanceState.getBoolean("transition_executed", false);
+        }
+
         loadImage();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean("transition_executed", isTransitionExecuted);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.viewer, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share:
+                share();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     View getSharedElement() {
@@ -98,7 +143,8 @@ public class ViewerFragment extends RxBindingFragment<ViewerFragmentBinding> {
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void loadImage() {
-        if (hasSharedElementTransition) {
+        if (hasSharedElementTransition && !isTransitionExecuted) {
+            isTransitionExecuted = true;
             loadThumbnail();
             EnterTransitionCompat.addListener(getActivity().getWindow(), new SimpleTransitionListener() {
                 @Override
@@ -147,6 +193,56 @@ public class ViewerFragment extends RxBindingFragment<ViewerFragmentBinding> {
                 binding.thumbnail.setVisibility(View.GONE);
             }
         }).start();
+    }
+
+    private void share() {
+        Observable.just(Glide.with(this).load(image.url))
+                .map(request -> {
+                    try {
+                        return request.downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL).get();
+                    } catch (InterruptedException | ExecutionException e) {
+                        return null;
+                    }
+                })
+                .filter(file -> file != null)
+                .map(src -> {
+                    // SB Chinese SNS apps, including Weibo, Wechat and QQ, don't read "content://"
+                    // uris, so we have to copy it to the external storage to generate a "file://"
+                    // uri.
+                    try {
+                        return copy(src, makeExternalFile(src.getName() + "." + image.meta.type));
+                    } catch (IOException e) {
+                        return null;
+                    }
+                })
+                .compose(bindToLifecycle())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(file -> {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("image/" + image.meta.type);
+                    intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+                    startActivity(Intent.createChooser(intent, getString(R.string.share_title)));
+                });
+    }
+
+    private File copy(File from, File to) throws IOException {
+        FileInputStream input = new FileInputStream(from);
+        FileOutputStream output = new FileOutputStream(to);
+
+        FileChannel inputChannel = input.getChannel();
+        FileChannel outputChannel = output.getChannel();
+
+        inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+
+        input.close();
+        output.close();
+
+        return to;
+    }
+
+    private File makeExternalFile(String name) throws IOException {
+        return new File(getContext().getExternalCacheDir(), name);
     }
 
 }
