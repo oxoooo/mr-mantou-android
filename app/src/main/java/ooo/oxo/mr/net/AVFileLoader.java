6ebd8100da8cd544c21209b74de8f9c8f1a3a9a3
XiNGRZ <chenxingyu92@gmail.com>
2016-03-13 18:34:22 +0800

    update copyright

--- app/src/main/java/ooo/oxo/mr/net/AVFileLoader.java
+++ app/src/main/java/ooo/oxo/mr/net/AVFileLoader.java
@@ -1,6 +1,6 @@
 /*
  * Mr.Mantou - On the importance of taste
- * Copyright (C) 2015  XiNGRZ <xxx@oxo.ooo>
+ * Copyright (C) 2015-2016  XiNGRZ <xxx@oxo.ooo>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
